4cdbc699c531e1a92474fb4a41b6658a55446c69
GILBERTO COSTA JUNIOR <gilberto.costa@fatec.sp.gov.br>
2020-05-26 20:33:29 -0300

    ds

--- app/api.py
+++ app/api.py
@@ -22,7 +22,8 @@ grupos = ['Grupo 1','Grupo 2','Grupo 3']
 
 class setDadosIniciais(Resource):
     def get(self):
-        db_string = "postgres://fatec:fatec@postgres:5432/pi"
+        db_string = "postgres://umecukbhukghpw:ab54f571331b41ff9fbf760434d3c944d342d90a8727dc4693a4960fdce036de@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d9428qgpam0o6u"
+        #db_string = "postgres://fatec:fatec@postgres:5432/pi"
         db = create_engine(db_string)  
         base = declarative_base()
 
