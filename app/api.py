0ca209036f78fd76cca4972d1f005fb27fb10294
Cesar Ando <andocesar@yahoo.com.br>
2020-06-04 21:29:08 -0300

    criacao da variavel com todos os filmes e script para conectar ao banco e inserir os mesmos

--- app/api.py
+++ app/api.py
@@ -20,6 +20,13 @@ test = '{"gender1": "action", "gender2": "terror", "gender3": "comedy"}'
 generos = ['Ação','Adulto','Aventura','Animação','Biografia','Comédia','Crime','Documentário','Drama','Família','Fantasia','Film Noir','Game show','História','Horror','Musical','Música','Mistério','Notícia','Reality show','Romance','Ficção científica','Curta-metragem','Esporte','Programa de entrevistas','Suspense','Guerra','Ocidental']
 grupos = ['Grupo 1','Grupo 2','Grupo 3']
 
+integrantes = [
+    {
+        grupo: 1,
+        filme: 'Persepolis'
+    }
+];
+
 class setDadosIniciais(Resource):
     def get(self):
         db_string = "postgres://fatec:fatec@postgres:5432/pi"
@@ -52,8 +59,8 @@ class setDadosIniciais(Resource):
             'Msg': 'Cadastrado com sucesso.'
         }
 
-class setGenero(Resource):
-    def get(self,usuario, genero):
+class setDadosIniciais(Resource):
+    def get(self):
         db_string = "postgres://fatec:fatec@postgres:5432/pi"
         db = create_engine(db_string)  
         base = declarative_base()
@@ -63,30 +70,49 @@ class setGenero(Resource):
             id = Column(Integer, primary_key=True)
             tipo = Column(String)
         
-        class UsuarioGenero(base):
-            __tablename__ = 'UsuarioGenero'
-            id_usuario = Column(Integer, primary_key=True)
-            id_genero = Column(Integer, primary_key=True)
+        class Usuario(base):
+            __tablename__ = 'Usuario'
+            id = Column(Integer, primary_key=True)
+            desc = Column(String)
 
         Session = sessionmaker(db)  
         session = Session()
         base.metadata.create_all(db)
 
-        # Read
-        genero_id = 0
-        generos_banco = session.query(Genero)  
-        for g in generos_banco:  
-            if(g.tipo==genero):
-                genero_id = g.id
-                break
-
-        # Create 
-        usuario_genero = UsuarioGenero(id_usuario=usuario, id_genero=genero_id)  
-        session.add(usuario_genero)  
+        # Create
+        for genero in generos:
+            g = Genero(tipo=genero)
+            session.add(g)
+        for grupo in grupos:
+            g = Usuario(desc=grupo)
+            session.add(g)
         session.commit()
         return {
-            'Msg': 'Genero '+genero+' cadastrado para o usuario '+usuario+'.'
+            'Msg': 'Cadastrado com sucesso.'
         }
+
+class classeTeste(Resource):
+    def get(self,usuario, genero):
+        db_string = "postgres://fatec:fatec@postgres:5432/pi"
+        db = create_engine(db_string)  
+        base = declarative_base()
+
+        class Filme(base):
+            __tablename__ = 'Filme'
+            id = Column(Integer, primary_key=True)
+            grupo = Column(String)
+            nome = Column(String)
+            filme = Column(String)
+
+        Session = sessionmaker(db)  
+        session = Session()
+        base.metadata.create_all(db)
+
+        for f in filmes:
+            filme = Filme(grupo=f['grupo'], nome=f['nome'], filme=f['filme'])
+            session.add(filme)
+
+        session.commit()
 
 class getFilm(Resource):
     def get(self):
