e5e613cee76f0bbf7a46db9accfd6479d8788abe
Cesar Ando <andocesar@yahoo.com.br>
2020-06-12 02:38:06 -0300

    Criação da rota chamando a api da IA

--- app/ia_fuzzy.py
+++ app/ia_fuzzy.py
@@ -1,9 +1,11 @@
-import api
+import api_imdb
+import filmes
 import numpy as np
 import skfuzzy as fuzzy
 from skfuzzy import control as ctrl
 from collections import Counter
 import matplotlib.pyplot as plt
+import random
 #pip install scikit-fuzzy
 #pip install matplotlib
 
@@ -19,7 +21,6 @@ import matplotlib.pyplot as plt
 ano = ctrl.Antecedent(np.arange(1920,2020,1),'ano')
 rank = ctrl.Antecedent(np.arange(0,100,1),'rank')
 recomendacao = ctrl.Consequent(np.arange(0,100,1),'recomendacao')
-
 
 #divide por 4 rank
 #divisão 2015-2020 novo . 2000 - 2015 medio menor que 2000 velho
@@ -45,23 +46,23 @@ recomendacao_simulacao = ctrl.ControlSystemSimulation(recomendacao_crtl)
 
 def listaFilmesSugestao (filme):
     lista_filmes = {'filme_rep': [], 'rating_rep': [], 'ano_rep': [], 'filme_rat':[], 'ano_rat':[], 'rating_rat':[]}
-    #var = api.buscaEssencialFilme(filme)
+    #var = api_imdb.buscaEssencialFilme(filme)
     #sug_film = rotten_tomatoes.buscaTopsFilmesPorGenero(var['genero'][0]) 
-    sug_film = api.filmeSugerido(filme)
+    sug_film = api_imdb.filmeSugerido(filme)
     if (len(sug_film['filme_rep'][0]) > 0):
         for filme in range (len(sug_film['filme_rep'][0])):
-            dados_filme = api.buscaEssencialFilme(sug_film['filme_rep'][0][filme])
+            dados_filme = api_imdb.buscaEssencialFilme(sug_film['filme_rep'][0][filme])
             lista_filmes['ano_rep'].append(dados_filme['ano'])
             media_score = (float(dados_filme['popularidade'][0]) * 10)
             lista_filmes['rating_rep'].append(media_score)
             lista_filmes['filme_rep'].append(sug_film['filme_rep'][0][filme])
     for filme in range (len(sug_film['filme_rat'][0])):
-        dados_filme = api.buscaEssencialFilme(sug_film['filme_rat'][0][filme])
+        dados_filme = api_imdb.buscaEssencialFilme(sug_film['filme_rat'][0][filme])
         lista_filmes['ano_rat'].append(dados_filme['ano'])
         media_score = (float(dados_filme['popularidade'][0]) * 10)
         lista_filmes['rating_rat'].append(media_score)
         lista_filmes['filme_rat'].append(sug_film['filme_rat'][0][filme])    
-    return lista_filmes  
+    return lista_filmes
 
 def filtroSugestao (filme):
     filmes_recomendacoes = {'filme_rep':[], 'recomen_rep':[], 'filme_rat':[], 'recomen_rat':[]}
@@ -85,6 +86,11 @@ def filtroSugestao (filme):
     maior_recomen_rat = max(filmes_recomendacoes['recomen_rat'], key=float)
     posicao = filmes_recomendacoes['recomen_rat'].index(maior_recomen_rat)
     nome_filme_rat = filmes_recomendacoes['filme_rat'][posicao]
-    return nome_filme_rep, nome_filme_rat
+    #return nome_filme_rep, nome_filme_rat
+    return nome_filme_rep
 
-print('\n \nO filme com maior recomendação é : ', filtroSugestao('Les Misérables'))
+#print('\n \nO filme com maior recomendação é : ', filtroSugestao('Les Misérables'))
+
+def getFilmeByGrupo(id):
+    filmesGrupo =  filmes.filmesGrupo();
+    return filtroSugestao(random.choice(filmesGrupo[id]))
