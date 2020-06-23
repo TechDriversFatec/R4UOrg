e9f3e44fd88d039f91bd811528e54bd051888a57
gilberto.junior <gilberto.junior@gsw.com.br>
2020-06-23 19:43:27 -0300

    Swagger ui

--- app/server.py
+++ app/server.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from threading import Thread
+from flask_swagger_ui import get_swaggerui_blueprint
 
 if (os.environ.get('ENV') != 'prod'):
     os.environ['DATABASE_URL'] = 'postgres://fatec:fatec@postgres:5432/pi'  
@@ -16,6 +17,23 @@ migrate = Migrate(app, db)
 port = int(os.environ.get("PORT", 5000))
 
 filmes = { "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": [] }
+
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+SWAGGER_URL =  '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Topicos Avançados"
+    }
+    
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 # Rota mockada sem processamento da IA para envio do filme.
 @app.route('/getFilme/<string:id>', methods=['GET'])
