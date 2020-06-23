ee4cdb9d7f70b2ac30590822dc56448133102aa6
Cesar Ando <andocesar@yahoo.com.br>
2020-06-23 02:32:38 -0300

    Incluido tratamento para evitar filmes repetidos e adicionado retorno do banco na rota getFilmeIA.

--- app/server.py
+++ app/server.py
@@ -28,10 +28,11 @@ class Filme(db.Model):
         return {
             "id": self.id,
             "grupo": self.grupo,
-            "name": self.nome
+            "nome": self.nome
         }
-
-filmes = { "1": [], "2": [], "3": [], "4": [], "5": [], "6": [], "7": [], "8": [], "9": [] }
+    
+    def getNome(self):
+        return self.nome
 
 @app.route('/getFilmes', methods=['GET'])
 def getFilmes():
@@ -39,7 +40,7 @@ def getFilmes():
 
 @app.route('/getFilmes/<int:grupo>', methods=['GET'])
 def getFilmesByGrupo(grupo):
-    return jsonify({'filmes': list(map(lambda filme: filme.serialize(), Filme.query.filter_by(grupo=str(grupo)).all()))})
+    return jsonify({'filmes': list(map(lambda filme: filme.getNome(), Filme.query.filter_by(grupo=str(grupo)).all()))})
 
 # Rota mockada sem processamento da IA para envio do filme.
 @app.route('/getFilme/<int:grupo>', methods=['GET'])
@@ -50,22 +51,26 @@ def getFilme(grupo):
 @app.route('/getFilmeIA/<int:grupo>', methods=['GET'])
 def getFilmeIA(grupo):
     if(grupo>0 and grupo<10):
-        return jsonify({"filme": "teste"})
+        filmesByGrupo = list(map(lambda filme: filme.getNome(), Filme.query.filter_by(grupo=str(grupo)).all()))
+        return jsonify({"filme": filmesByGrupo and random.choice(filmesByGrupo) or ""})
     else:
-        return jsonify({"Erro": "Número de grupo inválido."})
+        return jsonify({"Erro": "Número de grupo inválido, envie um número de 1 a 9."})
 
 @app.route('/getFilme/<string:grupo>', methods=['GET'])
 def getFilmeString(grupo):
-    return jsonify({"Erro": "Envie um número."})
+    return jsonify({"Erro": "Envie um número de 1 a 9."})
 
-def buscaFilme(grupo):
-    filme = Filme(grupo, ia_fuzzy.getFilmeByGrupo(grupo))
-    db.session.add(filme)
-    db.session.commit()
-    buscaFilme(grupo)
+def getFilmeByGrupo(grupo):
+    nome = ia_fuzzy.getFilmeByGrupo(grupo)
+    filmesByGrupo = list(map(lambda filme: filme.getNome(), Filme.query.filter_by(grupo=grupo).all()))
+    if(not nome in filmesByGrupo):
+        filme = Filme(grupo, nome)
+        db.session.add(filme)
+        db.session.commit()
+    getFilmeByGrupo(grupo)
 
 if __name__ == '__main__':
     for grupo in range(1,10):
-        thread = Thread(target=buscaFilme, args=(str(grupo),))
+        thread = Thread(target=getFilmeByGrupo, args=(str(grupo),))
         thread.start()
     app.run(debug=True, host='0.0.0.0', port=port)
