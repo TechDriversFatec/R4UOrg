08a33496f9a94f448649d7fad4eaf5bed11bd1fc
wesleyvs <42287703+wesleyvs@users.noreply.github.com>
2020-11-10 20:16:54 -0300

    Update deployTestConfig.sh

--- scripts/deployTestConfig.sh
+++ scripts/deployTestConfig.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-. /home/ubuntu/cron/docker_prune.sh
+sudo . /home/ubuntu/cron/docker_prune.sh
 
-docker stop $(docker ps -aq)
+sudo docker stop $(docker ps -aq)
 
-docker rm $(docker ps -aq)
+sudo docker rm $(docker ps -aq)
 
-docker rmi $(docker images -aq)
+sudo docker rmi $(docker images -aq)
 
 cd /home/ubuntu/r4u_application
 
-docker-compose -f docker-compose-migration.yml up -d
+sudo docker-compose -f docker-compose-migration.yml up -d
 
-docker-compose up -d
+sudo docker-compose up -d
 
-robot --outputdir /home/ubuntu/robotLogs --nostatusrc robotTests.robot
+sudo robot --outputdir /home/ubuntu/robotLogs --nostatusrc robotTests.robot
