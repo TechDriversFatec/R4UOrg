5572ba0d0ab6b73bf164d8e6907924370044df59
wesleyvs <42287703+wesleyvs@users.noreply.github.com>
2020-11-10 20:59:40 -0300

    Update deployTestConfig.sh

--- scripts/deployTestConfig.sh
+++ scripts/deployTestConfig.sh
@@ -14,4 +14,4 @@ docker-compose -f docker-compose-migration.yml up -d
 
 docker-compose up -d
 
-robot --outputdir /home/ubuntu/robotLogs --nostatusrc robotTests.robot
+robot --outputdir /home/ubuntu/robotLogs --nostatusrc tests/robotTests.robot
