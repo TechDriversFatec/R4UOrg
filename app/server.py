4e848e470946f6e397660fb3e5923ab3e5a9d492
wesleyvs <wesleyvini.social@gmail.com>
2020-11-23 21:39:39 -0300

    Update CORS Filter

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ if (os.environ.get('ENV') != 'prod'):
     os.environ['DATABASE_URL'] = 'postgres://fatec:fatec@postgres:5432/pi'
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": ["http://192.168.1.13:8080", "http://192.168.1.13:8081"]}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
